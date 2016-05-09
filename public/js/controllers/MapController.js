cfd79b97b951dfe38351d90156c2418f6aef7612
shishkina <shishkina.kate@gmail.com>
2016-05-09 17:29:22 -0400

    got the map to display with the location marker

--- public/js/controllers/MapController.js
+++ public/js/controllers/MapController.js
@@ -1,12 +1,72 @@
 'use strict'
 console.log("inside the MapController");
-	app.controller('MapController', function ($scope, $window){
+	app.controller('MapController', MapController);
+
+function 	MapController ($scope, $window){
   navigator.geolocation.getCurrentPosition(function (position){
     var lat = position.coords.latitude;
     var lng = position.coords.longitude;
+
     $scope.$apply(function(){
       $scope.lat = lat;
       $scope.lng = lng;
-    })
-  });
-});
+
+		var mapOptions = {
+			zoom: 12,
+			center: new google.maps.LatLng(lat, lng),
+			mapTypeId: google.maps.MapTypeId.ROADMAP,
+		};
+		$scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+		var createMarker = function(lat, lng){
+	  		var marker = new google.maps.Marker({
+    			map: $scope.map,
+    			position: new google.maps.LatLng(lat, lng),
+				});
+		}
+		createMarker($scope.lat, $scope.lng);
+		});
+	});
+
+// var initialLocation;
+// var siberia = new google.maps.LatLng(60, 105);
+// var newyork = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
+// var browserSupportFlag =  new Boolean();
+//
+// function initialize() {
+//   var myOptions = {
+//     zoom: 6,
+//     mapTypeId: google.maps.MapTypeId.ROADMAP
+//   };
+//   var map = new google.maps.Map(document.getElementById("map"), myOptions);
+//
+//   // Try W3C Geolocation (Preferred)
+//   if(navigator.geolocation) {
+//     browserSupportFlag = true;
+//     navigator.geolocation.getCurrentPosition(function(position) {
+//       initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+//       map.setCenter(initialLocation);
+//     }, function() {
+//       handleNoGeolocation(browserSupportFlag);
+//     });
+//   }
+//   // Browser doesn't support Geolocation
+//   else {
+//     browserSupportFlag = false;
+//     handleNoGeolocation(browserSupportFlag);
+//   }
+//
+//   function handleNoGeolocation(errorFlag) {
+//     if (errorFlag == true) {
+//       alert("Geolocation service failed.");
+//       initialLocation = newyork;
+//     } else {
+//       alert("Your browser doesn't support geolocation. We've placed you in Siberia.");
+//       initialLocation = siberia;
+//     }
+//     map.setCenter(initialLocation);
+//   }
+// }
+
+
+}
