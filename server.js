497513f369ecf12f6b5250f7b1192d78d05fc548
shishkina <shishkina.kate@gmail.com>
2016-05-14 17:21:17 -0400

    added location route, moved token verification to authorization_routes

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 var express = require('express');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
-var request = require('request');
-var bcrypt = require('bcrypt');
 var userRoutes = require('./config/user_routes.js');
 var authorizationRoutes = require('./config/authorization_routes.js');
 var weatherRoutes = require('./config/weather_routes.js');
+var locationRoutes = require('./config/location_routes.js');
 var mongoose = require('mongoose');
-var jwt = require('jsonwebtoken');
 var User = require('./models/user.js');
 var app = express();
 
@@ -25,38 +23,11 @@ app.get('/', function(req, res){
   res.render('public/index.html');
 });
 
-//authentication handling
-var secret = 'Sunshine';
 app.use('/authenticate', authorizationRoutes);
 app.use('/user', userRoutes);
-app.use('/currentWeather', weatherRoutes);
+app.use('/weather', weatherRoutes);
+app.use('/location', locationRoutes);
 
-app.use(function(req, res, next){
-  //check header or url parameters or post parameters for token
-  var token = req.body.token || req.query.token || req. headers['x-access-token'];
-  //decode token
-  if(token){
-    //verifies secret
-    jwt.verify(token, 'Sunshine', function(err, decoded){
-      if(err){
-        return res.json({
-          success: false,
-          message: 'Failed to authenticate token.'
-        });
-      } else {
-        //if everything os ok, save to request for use in other routes
-        req.decoded = decoded;
-        next();
-      }
-    });
-  } else {
-    // if there is no token, return an error
-    return res.status(403).send({
-      success: false,
-      message: 'No token provided.'
-    });
-  }
-});
 //connect to the database
 mongoose.connect('mongodb://localhost/weatherApp', function(err){
   if(err){
