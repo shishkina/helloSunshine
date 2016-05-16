6be86eea4c2d0dee3f9e01df98819116581b0063
shishkina <shishkina.kate@gmail.com>
2016-05-16 16:38:01 -0400

    provided API keys as environmental variables

--- config/location_routes.js
+++ config/location_routes.js
@@ -5,13 +5,15 @@ var bodyParser = require('body-parser');
 var LocationController = require('../controllers/location.js');
 var request = require('request');
 
+const GOOGLE_KEY = process.env.GOOGLE_KEY;
+
 router.route('/:searchLocation')
   .get(function(req, res){
     var searchLocation = req.params.searchLocation;
-    var decode = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + searchLocation + '&key=AIzaSyARFlpAtezjAvQvEbDyIu6XJ2BwLdIFhyo';
+    var decode = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + searchLocation + '&key=' + GOOGLE_KEY;
     LocationController.getLocation(decode, function(body){
       res.json(JSON.parse(body));
     });
   });
-  
+
 module.exports = router;
--- config/weather_routes.js
+++ config/weather_routes.js
@@ -4,14 +4,19 @@ var bodyParser = require('body-parser');
 var WeatherController = require('../controllers/weather.js');
 var request = require('request');
 
+const WEATHER_KEY = process.env.WEATHER_KEY;
 
 router.get('/:lat/:lng', function(req, res){
     var latitude = req.params.lat;
     var longitude = req.params.lng;
-    var weatherUrl = 'https://api.forecast.io/forecast/25754e91869bd12b673bf2e223fc76c6/' + latitude + ',' + longitude;
-
+    console.log("get/weather before the api call" );
+    console.log("this is key " + WEATHER_KEY);
+    var weatherUrl = 'https://api.forecast.io/forecast/' + WEATHER_KEY + '/' + latitude + ',' + longitude;
+    console.log("right after the api call");
     WeatherController.getWeather(weatherUrl, function(body){
-      res.json(JSON.parse(body));
+      console.log("inside the WeatherController, before JSON");
+      console.log(body + " body");
+      res.send(JSON.parse(body));
     });
   });
 
